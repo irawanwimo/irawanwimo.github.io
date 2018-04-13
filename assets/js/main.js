fe373211237143442288a929bdb1088fff64fee7
irawanwimo <irawandwihastomo@ymail.com>
2018-04-13 16:51:11 +0700

    modify loader

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,10 +2,10 @@
  * Preloader
  /* ---------------------------------------------- */
 (function(){
-    $(window).on('load', function() {
+    // $(window).on('load', function() {
         $('.loader').fadeOut();
         $('.page-loader').delay(350).fadeOut('slow');
-    });
+    // });
 
     $(document).ready(function() {
 
