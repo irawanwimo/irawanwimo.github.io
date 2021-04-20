38a178e63da3057a7c46c5fd5cdd3d2553506f21
irawanwimo <irawandwihastomo@ymail.com>
2021-04-20 19:01:01 +0700

    Fix mobile

--- assets/js/wimo.js
+++ assets/js/wimo.js
@@ -3,6 +3,11 @@ mobile_check = $( window ).width();
 if(mobile_check < 769){
     $(document.body).addClass("body_mobile");
     $(document.body).css("overflow", "hidden");
+
+    $('.boxarea').addClass("max-h-400");
+}else{
+    $('.cont-box').addClass("margin-t-70");
+    $('.boxarea').addClass("max-h-500");
 }
 
 $(".slider ol li").on("click", function() {
