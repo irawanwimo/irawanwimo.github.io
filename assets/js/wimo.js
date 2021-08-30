02953bc5fef27c4bcb4f6fa5e9a8f5501ec37e70
irawanwimo <irawandwihastomo@ymail.com>
2021-08-30 19:18:00 +0700

    test fix mobile

--- assets/js/wimo.js
+++ assets/js/wimo.js
@@ -1,11 +1,13 @@
 mobile_check = $( window ).width();
 
 if(mobile_check < 769){
+    console.log(mobile_check)
     $(document.body).addClass("body_mobile");
-    $(document.body).css("overflow", "hidden");
+    // $(document.body).css("overflow", "hidden");
 
     $('.boxarea').addClass("max-h-400");
 }else{
+    console.log(mobile_check)
     $('.cont-box').addClass("margin-t-70");
     $('.boxarea').addClass("max-h-500");
 }
