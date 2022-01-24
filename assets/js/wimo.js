d7d8602d1d37fc99f49b6e6b4baa50a4ce6a3b95
irawanwimo <irawandwihastomo@ymail.com>
2022-01-24 21:31:53 +0700

    fix mobile js

--- assets/js/wimo.js
+++ assets/js/wimo.js
@@ -3,19 +3,9 @@ mobile_check = $( window ).width();
 if(mobile_check < 769){
     console.log(mobile_check)
     $(document.body).addClass("body_mobile");
-    $(document.body).css("overflow", "hidden");
-
-    $('.boxarea').addClass("max-h-400");
+    $(document.body).css("overflow-x", "hidden");
 }else{
     console.log(mobile_check)
-    $('.cont-box').addClass("margin-t-70");
-    $('.boxarea').addClass("max-h-500");
 }
 
-$(".slider ol li").on("click", function() {
-    $(this).addClass("active").siblings("li").removeClass("active");
-    $(".slider ul").animate({
-        top: -$(".slider").height() * $(this).index()
-    }, 500);
-});
 
